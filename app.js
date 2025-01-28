93123427e0f076c66113828551c9fa06058482e2
Stephen Bowen <sbowen@architectnow.net>
2025-01-28 10:57:16 -0600

    rework app.js

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
-import dotenv from "dotenv";
-dotenv.config();
-
-import express from "express";
-import helmet from "helmet";
-import morgan from "morgan";
-import bodyParser from "body-parser";
-import cors from "cors";
-
+const express = require("express");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
@@ -16,6 +12,38 @@ app.use(morgan("common"));
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(cors());
 
-const PORT = process.env.PORT || 8080;
+// This is your test secret API key.
+const stripe = require("stripe")('sk_test_51Qm88ER2BhZW0JUn8B2heO63rfQ05m2NUXt2JUab0uxG6Tx2SM5WX2IUlLAAtciPCDUjMKiuKBC1NVjf5nlcEF7b00pM82ctqI');
 
-app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+app.use(express.static("public"));
+
+const calculateOrderAmount = (items) => {
+  // Calculate the order total on the server to prevent
+  // people from directly manipulating the amount on the client
+  let total = 0;
+  items.forEach((item) => {
+    total += item.amount;
+  });
+  return total;
+};
+
+app.post("/create-payment-intent", async (req, res) => {
+  const { items } = req.body;
+
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: calculateOrderAmount(items),
+    currency: "usd",
+    // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  });
+
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
+
+
+app.listen(4242, () => console.log("Node server listening on port 4242!"));
